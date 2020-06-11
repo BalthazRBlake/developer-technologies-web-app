9e0b0e4b6a622bdd788e46c158980a0a8d4a5aa9
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-11 17:41:19 +0300

    Set middlewares for server use

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const server = express();
+const cors = require("cors");
+
+server.use(express.json());
+server.use(express.static(__dirname + "/../public"));
+server.use(cors());
 
 module.exports = server;
