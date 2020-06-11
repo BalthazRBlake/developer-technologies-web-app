212ee6fafdcff32324c27aa963678eee9279ea55
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-11 18:03:06 +0300

    server GET enpoint to get all entries in MongoDB

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
+const { TechnologyModel } = require("../models");
 
 server.use(express.json());
 server.use(express.static(__dirname + "/../public"));
 server.use(cors());
+
+server.get("/api/technologies", async (request, response) => {
+  let technologies = await TechnologyModel.find();
+
+  technologies = technologies.map(technology => {
+    technologies.logo = `${request.protocol}://${request.headers.host}/img/${technology.logo}`;
+    return technology;
+  });
+
+  return response.send({ error: false, data: technologies });
+});
 
 module.exports = server;
