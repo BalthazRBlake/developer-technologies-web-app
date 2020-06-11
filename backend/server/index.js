f8d7681092c9cff559d8fe861d06ba5c5a83072b
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-11 18:20:16 +0300

    GET enpoint findById

--- backend/server/index.js
+++ backend/server/index.js
@@ -18,4 +18,13 @@ server.get("/api/technologies", async (request, response) => {
   return response.send({ error: false, data: technologies });
 });
 
+server.get("/api/technology/:id", async (request, response) => {
+  const { id } = request.params;
+  
+  let technology = await TechnologyModel.findById(id);
+  technology.logo = `${request.protocol}://${request.headers.host}/img/${technology.logo}`;
+
+  return response.send({ error: false, data: technology });
+});
+
 module.exports = server;
