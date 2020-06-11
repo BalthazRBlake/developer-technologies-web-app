29eb3f75bd4f7719bd738314587c10fae0c4da13
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-11 17:28:21 +0300

    Connect to MongoDB and run server

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const server = require("./server");
-const { PORT } = require("./config");
+const { PORT, MONGO_URI } = require("./config");
+const mongoose = require("mongoose");
 
-server.listen(PORT, () => {
-  console.log(`Backend up an running in PORT ${PORT}`);
-});
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Backend up an running in PORT ${PORT}`);
+    });
+  })
+  .catch(console.log);
